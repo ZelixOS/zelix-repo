c3edfaa9844b71ea71f1816704da73cb824c1c34
lanierc <yasirzengin10@gmail.com>
2026-08-30 00:48:46 +0300

    Add plasma update script and autostart to guarantee Zelix Aurora wallpaper is applied on first boot

---
+++ zelixins/zelixdeps/usr/share/plasma/shells/org.kde.plasma.desktop/contents/updates/00-zelix-wallpaper.js
@@ -0,0 +1,8 @@
+var allDesktops = desktops();
+for (var i = 0; i < allDesktops.length; i++) {
+    var d = allDesktops[i];
+    d.wallpaperPlugin = "org.kde.image";
+    d.currentConfigGroup = ["Wallpaper", "org.kde.image", "General"];
+    d.writeConfig("Image", "/usr/share/wallpapers/Zelix/ZelixOS Aurora.png");
+    d.writeConfig("FillMode", 2);
+}
